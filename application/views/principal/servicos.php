51985a5c7dfd12b33cc552756fccce50133812b3
Cássio Araujo <caraujo@inf.ufsm.br>
2018-09-14 16:07:43 -0300

    Correção do endereço em serviços

--- application/views/principal/servicos.php
+++ application/views/principal/servicos.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <!-- Latest compiled and minified CSS and JS -->
-<link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url();?>assets//assets/img/logo.png"/>
+<link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url();?>/assets/img/logo.png"/>
 <link rel="stylesheet" href="<?php echo base_url();?>css/reset.css"/>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
